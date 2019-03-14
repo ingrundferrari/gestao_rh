55b482024ccdb77ec5cd75bc3705189ed9e07804
MarcoFerrari <ingrund.ferrari@gmail.com>
2019-03-14 16:52:37 -0300

    crud funcionarios

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from apps.departamentos.models import Departamento
 from apps.empresas.models import Empresa
-
+from django.urls import reverse
 
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100)
@@ -11,5 +11,8 @@ class Funcionario(models.Model):
     empresa = models.ForeignKey(
         Empresa, on_delete=models.PROTECT, null=True, blank=True)
 
+    def get_absolute_url(self):
+       return reverse('list_funcionarios') 
+       
     def __str__(self):
         return self.nome
--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,5 +1,27 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.views.generic import ListView, UpdateView, DeleteView, CreateView
+from .models import Funcionario
+from django.urls import reverse_lazy
 
-def home(request):
-    return HttpResponse('olá')
+
+class FuncionariosList(ListView):
+    model = Funcionario
+
+    def get_queryset(self):
+        empresa_logada = self.request.user.funcionario.empresa    
+        return Funcionario.objects.filter(empresa=empresa_logada)
+
+
+class FuncionarioEdit(UpdateView):
+    model = Funcionario
+    fields = ['nome', 'departamentos'] 
+
+
+class FuncionarioDelete(DeleteView):
+     model = Funcionario
+     success_url =  reverse_lazy('list_funcionarios')
+     
+     
+class FuncionarioNovo(CreateView):
+     model = Funcionario
+     fields = ['nome', 'departamentos']
+
