a92c779d08530995eda31e8add37e82ba3ad14b6
MarcoFerrari <ingrund.ferrari@gmail.com>
2019-03-15 01:10:16 -0300

    correção crud funcionarios

--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,6 +1,10 @@
+from django.views.generic.base import View, TemplateView
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 from django.views.generic import ListView, UpdateView, DeleteView, CreateView
 from .models import Funcionario
 from django.urls import reverse_lazy
+
 
 
 class FuncionariosList(ListView):
@@ -19,9 +23,15 @@ class FuncionarioEdit(UpdateView):
 class FuncionarioDelete(DeleteView):
      model = Funcionario
      success_url =  reverse_lazy('list_funcionarios')
-     
+
      
 class FuncionarioNovo(CreateView):
      model = Funcionario
      fields = ['nome', 'departamentos']
 
+     def form_valid(self, form):
+         funcionario = form.save(commit=False)
+         username = funcionario.nome.split(' ')[0] + funcionario.nome.split(' ')[1]
+         funcionario.empresa = self.request.user.funcionario.empresa
+         funcionario.user = User.objects.create(username=username)
+         return super(FuncionarioNovo, self).form_valid(form)
