313216a866e8ef5429f0b41b0b5b7ca64166a4b8
MarcoFerrari <ingrund.ferrari@gmail.com>
2019-03-23 15:21:30 -0300

    marco

--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from .models import RegistroHoraExtra
+#from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import ListView
+#, UpdateView, DeleteView, CreateView
+#from django.urls import reverse_lazy
 
-# Create your views here.
+
+class HoraExtraList(ListView):
+    model = RegistroHoraExtra
+
+    def get_queryset(self):
+        empresa_logada = self.request.user.funcionario.empresa
+        return RegistroHoraExtra.object.filter(funcionario__empresa=empresa_logada)
